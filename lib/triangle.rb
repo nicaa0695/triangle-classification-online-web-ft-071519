e5137798176ec9958109172d6688dc8c27f6cdd8
Veronica Lopez <nicaa0695@gmail.com>
2019-08-01 22:56:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,10 @@
 class Triangle
-  # write code here
+  attr_accessor :first_length, :sencond_length, :third_length
+  def initialize(first_length:, sencond_length:, third_length:)
+    @first_length = first_length 
+    @sencond_length = sencond_length 
+    @third_length = third_length
+  end 
+  def kind
+    
 end
